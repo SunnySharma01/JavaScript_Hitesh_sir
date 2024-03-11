25f627a2bfa2608f6cd0f6d6e4dd2fabbf4f8d91
Sunny Kumar <125676565+SunnySharma01@users.noreply.github.com>
2024-03-11 17:26:13 +0530

    comparison

--- 01_Basic/04_Comparision.js
+++ 01_Basic/04_Comparision.js
@@ -5,8 +5,8 @@
 // console.log(2 != 1);
 
 
-// console.log("2" > 1);
-// console.log("02" > 1);
+ console.log("2" > 1);
+ console.log("02" > 1);
 
 console.log(null > 0);
 console.log(null == 0);
@@ -16,6 +16,6 @@ console.log(undefined == 0);
 console.log(undefined > 0);
 console.log(undefined < 0);
 
-// === 
+// === scrict comparision
 
 console.log("2" === 2);
