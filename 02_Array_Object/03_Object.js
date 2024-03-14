a25e8c7c8f45736f1b195244b37224662ee9b507
Sunny Kumar <125676565+SunnySharma01@users.noreply.github.com>
2024-03-14 17:54:35 +0530

    objectBasic

---
+++ 02_Array_Object/03_Object.js
@@ -0,0 +1,38 @@
+// singleton
+// Object.create
+
+// object literals
+
+const mySym = Symbol("key1")
+
+
+const JsUser = {
+    name: "Hitesh",
+    "full name": "Hitesh Choudhary",
+    [mySym]: "mykey1",                  //Symbol Notation
+    age: 18,
+    location: "Jaipur",
+    email: "hitesh@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+
+// console.log(JsUser.email)             //Print 1 way object
+// console.log(JsUser["email"])         //Print 2 way object 
+// console.log(JsUser["full name"])
+// console.log(JsUser[mySym])          //Symbol printing method
+
+JsUser.email = "hitesh@chatgpt.com"
+// Object.freeze(JsUser)
+JsUser.email = "hitesh@microsoft.com"
+// console.log(JsUser);
+
+JsUser.greeting = function(){                        //greeting massage
+    console.log("Hello JS user");
+}
+JsUser.greetingTwo = function(){
+    console.log(`Hello JS user, ${this.name}`);
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
