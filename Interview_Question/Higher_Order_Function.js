36ace145c000e6724f5fd291316907709941c005
Sunny Kumar <125676565+SunnySharma01@users.noreply.github.com>
2024-03-18 18:48:53 +0530

    Interview

--- Interview_Question/Higher_Order_Function.js
+++ Interview_Question/Higher_Order_Function.js
@@ -32,3 +32,17 @@ const returnedFunction = higherOrderFunction();
 
 // Call the returned function
 returnedFunction();
+
+
+//
+let a = 10, b = 10, 
+c = 10;
+
+if (a == b && b == c)    //(===)
+{
+    console.log("Hello");
+} 
+else 
+{
+    console.log("Bye Bye");
+}
